f743efe121186cfa473d8d1bce829c1fbf5886a3
Damir <tebuchet@gmail.com>
2017-10-08 15:18:56 +0300

    Application

--- src/main/java/ru/damirmanapov/Application.java
+++ src/main/java/ru/damirmanapov/Application.java
@@ -6,10 +6,10 @@ import org.springframework.boot.context.properties.EnableConfigurationProperties
 
 @EnableConfigurationProperties
 @SpringBootApplication
-public class MicroserviceTemplateApplication {
+public class Application {
 
     public static void main(String[] args) {
-        SpringApplication.run(MicroserviceTemplateApplication.class, args);
+        SpringApplication.run(Application.class, args);
     }
 
 }
