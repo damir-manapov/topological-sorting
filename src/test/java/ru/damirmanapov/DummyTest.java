a55f9f81afb53873d62ea50efa4703464d5651f0
Damir <tebuchet@gmail.com>
2017-10-08 16:20:33 +0300

    DummyTest

--- src/test/java/ru/damirmanapov/DummyTest.java
+++ src/test/java/ru/damirmanapov/DummyTest.java
@@ -1,23 +1,25 @@
 package ru.damirmanapov;
 
+import org.testng.annotations.Test;
+
 import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.samePropertyValuesAs;
 
-@org.testng.annotations.Test(groups = "integration")
-public class Test {
+@Test
+public class DummyTest {
 
-    @org.testng.annotations.Test
+    @Test
     public void testIs() {
         assertThat(true, is(true));
     }
 
-    @org.testng.annotations.Test
+    @Test
     public void testSamePropertyValuesAs() {
 
-        TestEntity expectedTestEntity = new TestEntity("Test Name");
+        TestEntity expectedTestEntity = new TestEntity("DummyTest Name");
 
-        TestEntity actualTestEntity = new TestEntity("Test Name");
+        TestEntity actualTestEntity = new TestEntity("DummyTest Name");
 
         assertThat(actualTestEntity, samePropertyValuesAs(expectedTestEntity));
     }
