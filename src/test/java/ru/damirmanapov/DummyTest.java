5433900baa901bebf493548e9620b2b2f03d2521
Damir <tebuchet@gmail.com>
2017-10-09 16:06:45 +0300

    jopt

--- src/test/java/ru/damirmanapov/DummyTest.java
+++ src/test/java/ru/damirmanapov/DummyTest.java
@@ -1,13 +1,29 @@
 package ru.damirmanapov;
 
+import joptsimple.OptionParser;
+import joptsimple.OptionSet;
+import joptsimple.OptionSpec;
 import org.testng.annotations.Test;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.samePropertyValuesAs;
+import static org.testng.Assert.assertTrue;
+import static org.testng.AssertJUnit.assertEquals;
 
 @Test
 public class DummyTest {
+
+    public static <T> List<T> asList(T... elements) {
+        ArrayList<T> result = new ArrayList<>(elements.length);
+        for (T val : elements) {
+            result.add(val);
+        }
+        return result;
+    }
 
     @Test
     public void testIs() {
@@ -24,4 +40,29 @@ public class DummyTest {
         assertThat(actualTestEntity, samePropertyValuesAs(expectedTestEntity));
     }
 
+    @Test
+    public void testSamePropertyValuesAs___() {
+        OptionParser parser = new OptionParser();
+        OptionSpec<Integer> port = parser.accepts("port").withRequiredArg().ofType(Integer.class);
+        OptionSpec<Void> knownPeerPorts = parser.accepts("known-peer-ports");
+        OptionSpec<String> ports = parser.nonOptions().ofType(String.class);
+
+        OptionSet options = parser.parse("--port", "33333", "--known-peer-ports", "44444", "55555");
+
+        assertTrue(options.has(knownPeerPorts));
+
+        assertTrue(options.has(port));
+        assertTrue(options.hasArgument(port));
+        Integer expectedCount = 33333;
+        assertEquals(expectedCount, options.valueOf(port));
+        assertEquals(expectedCount, port.value(options));
+        assertEquals(asList(expectedCount), options.valuesOf(port));
+        assertEquals(asList(expectedCount), port.values(options));
+        assertEquals(asList("44444", "55555"), options.valuesOf(ports));
+
+        assertEquals(asList("44444", "55555"), ports.values(options));
+
+    }
 }
+
+
